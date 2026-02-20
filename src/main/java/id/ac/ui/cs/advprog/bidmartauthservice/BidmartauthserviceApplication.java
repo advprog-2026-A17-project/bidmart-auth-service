7b7641263a8c29b72905dedb3f526e225b0d10ac
Whosmomo <pradiptaadinugroho@gmail.com>
2026-02-20 08:46:41 +0700

    chore: initial setup for authentication service

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmartauthservice/BidmartauthserviceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartauthserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartauthserviceApplication.class, args);
+    }
+
+}
