4e2a7f428ec7faf2b924d1aedc8061981e82117f
aldofahrezy <aldofhrzy@gmail.com>
2026-04-13 15:45:07 +0700

    test: [GREEN] implement RBAC permission model and check endpoint

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmartauthservice/repository/PermissionRepository.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.bidmartauthservice.repository;
+
+import id.ac.ui.cs.advprog.bidmartauthservice.model.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PermissionRepository extends JpaRepository<Permission, UUID> {
+    Optional<Permission> findByName(String name);
+}
